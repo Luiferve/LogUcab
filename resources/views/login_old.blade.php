39c60f335d081e1d51113d1d16c71e0457e816f9
oswaldo <ojlr.1998@gmail.com>
2018-12-12 19:40:31 -0400

    New login interface

---
+++ resources/views/login_old.blade.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <!--====== USEFULL META ======-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Transportation & Agency Template is a simple Smooth transportation and Agency Based Template" />
+    <meta name="keywords" content="Portfolio, Agency, Onepage, Html, Business, Blog, Parallax" />
+
+    <!--====== TITLE TAG ======-->
+    <title>LogUcab 
+        @if (!empty($users))
+            {{$users[0]->usu_email}}
+            {{Cookie::queue('_token', $users[0]->usu_email, 60)}}
+        @endif
+    </title>
+
+    <!--====== FAVICON ICON =======-->
+    <link rel="shortcut icon" type="image/ico" href="img/favicon.png" />
+
+    <!--====== STYLESHEETS ======-->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/stellarnav.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!--====== MAIN STYLESHEETS ======-->
+    <link href="style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body class="home-one">
+
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!--- PRELOADER -->
+    <div class="preeloader">
+        <div class="preloader-spinner"></div>
+    </div>
+
+    <!--SCROLL TO TOP-->
+    <a href="#home" class="scrolltotop"><i class="fa fa-long-arrow-up"></i></a>
+
+    <!--START TOP AREA-->
+    <header class="top-area" id="home">
+        <div class="top-area-bg" data-stellar-background-ratio="0.6"></div>
+        <div class="header-top-area">
+            <!--MAINMENU AREA-->
+            <div class="mainmenu-area" id="mainmenu-area">
+                <div class="mainmenu-area-bg"></div>
+                <nav class="navbar">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a href="{{url('/')}}" class="navbar-brand"><img src="img/logo.png" alt="logo"></a>
+                        </div>
+                        <div class="search-and-language-bar pull-right">
+                            <ul>
+                                <li><a href="{{url('/login')}}"><i class="fa fa-user"></i></a></li>
+                                <li class="search-box"><i class="fa fa-search"></i></li>
+                                <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+                                <li class="select-language">
+                                    <select name="#" id="#">
+                                    <option selected value="End">ENG</option>
+                                    <option value="ARA">ARA</option>
+                                    <option value="CHI">CHI</option>
+                                </select>
+                                </li>
+                            </ul>
+                            <form action="#" class="search-form">
+                                <input type="search" name="search" id="search">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                        <div id="main-nav" class="stellarnav">
+                            <ul id="nav" class="nav navbar-nav">
+                                <li><a href="{{url('/')}}">home</a>
+                                    <ul>
+                                        <li><a href="{{url('/')}}">Home Version 1</a></li>
+                                        <li><a href="index-2.html">Home Version 2</a></li>
+                                        <li><a href="index-3.html">Home Version 3</a></li>
+                                        <li><a href="index-4.html">Home Version 4</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">about</a>
+                                    <ul>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="about-company-profile.html">About Profile</a></li>
+                                        <li><a href="about-company-history.html">About History</a></li>
+                                        <li><a href="about-company-report.html">About Report</a></li>
+                                        <li><a href="about-team.html">About Team</a></li>
+                                        <li><a href="about-support.html">About Support</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="service.html">Service</a>
+                                    <ul>
+                                        <li><a href="service.html">Service Version 1</a></li>
+                                        <li><a href="service-2.html">Service Version 2</a></li>
+                                        <li><a href="service-3.html">Service Version 3</a></li>
+                                        <li><a href="single-service.html">Service Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Other Pages</a>
+                                    <ul>
+                                        <li><a href="404.html">404</a></li>
+                                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog.html">Blog</a>
+                                    <ul>
+                                        <li><a href="blog.html">Blog Version 1</a></li>
+                                        <li><a href="blog-2.html">Blog Version 2</a></li>
+                                        <li><a href="single-blog.html">Single Blog</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact</a>
+                                    <ul>
+                                        <li><a href="contact.html">Contact Version 1</a></li>
+                                        <li><a href="contact-2.html">Contact Version 2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <!--END MAINMENU AREA END-->
+        </div>
+        <!--START LOGIN-->
+        @if (empty($users))
+            <div class="container" id="logger">
+                @crsf
+                <form class="form-horizontal" method="POST" action="{{url('/login')}}">
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="email">Email:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" placeholder="Enter email" name = 'email'>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">Password:</label>
+                        <div class="col-sm-10"> 
+                            <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="pwd" placeholder="Enter password" name = 'password'>
+                        </div>
+                    </div>
+                    <div class="form-group"> 
+                            <div class="col-sm-offset-2 col-sm-10">
+                            <div class="checkbox">
+                                <label><input type="checkbox"> Remember me</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group"> 
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Login</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        @else
+            <div class="container" id="alert">
+                <div class="alert alert-success" role="alert">
+                    Bienvenido {{$users[0]->usu_nombre}}!!
+                </div>
+            </div>
+        @endif
+        <!--END LOGIN-->
+    </header>
+    <!--END TOP AREA-->
+
+    <!--FOOER AREA-->
+    <div class="footer-area dark-bg">
+        <div class="footer-copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+                        <div class="footer-copyright wow fadeIn">
+                            
+                            <p>Copyright 2018 All rights reserved | LogUcab <i class="fa fa-heart-o" aria-hidden="true"></i></p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+                        <div class="footer-social-bookmark text-right wow fadeIn">
+                            <ul class="social-bookmark">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--FOOER AREA END-->
+
+
+    <!--====== SCRIPTS JS ======-->
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <!--====== PLUGINS JS ======-->
+    <script src="js/vendor/jquery.easing.1.3.js"></script>
+    <script src="js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/vendor/jquery.appear.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/stellarnav.min.js"></script>
+    <script src="js/contact-form.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+
+    <!--===== ACTIVE JS=====-->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,12 @@ Route::get('/login', function () {
 
 Route::post('/login', function () {
     $name = 'post';
-    $users = DB::select('select usu_nombre from usuario where usu_codigo = '.$_POST['email'].' and usu_password = \''.$_POST['password'].'\'');
-    Cookie::queue('_token', 'test-123456789', 60);
-    return view('login',['name' => $name, 'users' => $users]);
+    $users = DB::select('select usu_email from usuario where usu_email = \''.$_POST['email'].'\' and usu_password = \''.$_POST['password'].'\'');
+    $message = 'Bienvenido '.$_POST['email'].'.';
+    if (empty($users)){
+        $message = 'Usuario ('.$_POST['email'].') no encontrado.';
+    }
+    
+    // Cookie::queue('_token', 'test-123456789', 60);
+    return view('login',['name' => $name, 'users' => $users, 'message' => $message]);
 });
