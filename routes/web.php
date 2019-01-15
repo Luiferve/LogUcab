3b388d494f17532512dd1332444534d9050245b2
oswaldo <ojlr.1998@gmail.com>
2019-01-15 12:56:59 -0400

    added note

--- routes/web.php
+++ routes/web.php
@@ -940,7 +940,7 @@ Route::get('/roles/delete/{id}', function($id) {
 })->where('id', '[0-9]+');
 
 Route::get('/report/port-franchises', function() {
-    //TODO:
+    //TODO:missing tables (aeropuerto y puerto)
     $franchises = DB::select('select aer_codigo cod,suc_codigo from aeropuerto,sucursal where aer_sucursal=suc_codigo union select pue_codigo cod,suc_codigo from puerto,sucursal where pue_sucursal=suc_codigo');
 
     audit(2,'Reporte Sucursales en Puertos y Aeropuertos');
